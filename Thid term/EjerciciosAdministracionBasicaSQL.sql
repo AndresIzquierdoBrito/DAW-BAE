4b7976cc9071dd48d19ec691a49b908451e3a44c
Andres Izquierdo <andres.izbri@gmail.com>
2023-03-28 13:31:36 +0100

    Small canges in AdmSQL

--- Thid term/EjerciciosAdministracionBasicaSQL.sql
+++ Thid term/EjerciciosAdministracionBasicaSQL.sql
@@ -1,205 +1,194 @@
 --1. Crear la Base de datos en la carpeta SQLDatos con tamaño inicial 5M, tamaño mayor
 --20 M e incrementos 4M.
-IF db_id('AdministracionSQL') IS NOT NULL
-	DROP DATABASE AdministracionSQL;
+IF Db_id('AdministracionSQL') IS NOT NULL
+  DROP DATABASE administracionsql;
 go
+
 -- Recuerda darle permisos a la carpeta con chmod
-CREATE DATABASE AdministracionSQL
-ON 
-( NAME = AdministracionSQL_dat,
-  FILENAME = '/SQLDatos/AdministracionSQL.mdf',
-  SIZE = 5MB,
-  MAXSIZE = 20MB,
-  FILEGROWTH = 4MB)
-LOG ON
-( NAME = AdministracionSQL_LOG,
-  FILENAME = '/SQLDatos/AdministracionSQL_log.ldf',
-  SIZE = 5MB,
-  MAXSIZE = 25MB,
-  FILEGROWTH = 5MB);
+CREATE DATABASE administracionsql ON ( NAME = administracionsql_dat, filename =
+'/SQLDatos/AdministracionSQL.mdf', size = 5mb, maxsize = 20mb, filegrowth = 4mb)
+log ON ( NAME = administracionsql_log, filename =
+'/SQLDatos/AdministracionSQL_log.ldf', size = 5mb, maxsize = 25mb, filegrowth =
+5mb);
 
-use AdministracionSQL;
-go
---2. Crear una tabla con la estructura siguiente:
-create table empleados (
-DNI varchar(8),
-nombre varchar(30),
-apellidos varchar(30),
-fechanacimiento datetime,
-cantidadhijos tinyint,
-seccion varchar(20),
-sueldo decimal(6,2)
-);
+USE administracionsql;
 
-exec sp_helpconstraint empleados;
 go
---3. Crear las sentencias para que valide lo siguiente:
+
+--2. Crear una tabla con la estructura siguiente:
+CREATE TABLE empleados
+  (
+     dni             VARCHAR(8),
+     nombre          VARCHAR(30),
+     apellidos       VARCHAR(30),
+     fechanacimiento DATETIME,
+     cantidadhijos   TINYINT,
+     seccion         VARCHAR(20),
+     sueldo          DECIMAL(6, 2)
+  );
+
+EXEC Sp_helpconstraint
+  empleados;
+
+go
+
+--3. Crear las sentencias para que valide lo siguiente:
 -- a. Clave primara DNI
 -- Pimera manera
-create table empleados (
-DNI varchar(8) primary key
-);
+CREATE TABLE empleados
+  (
+     dni VARCHAR(8) PRIMARY KEY
+  );
 
 -- Segunda manera
-create table empleados (
-DNI varchar(8),
-constraint pk_empleados primary key(dni)
-);
+CREATE TABLE empleados
+  (
+     dni VARCHAR(8),
+     CONSTRAINT pk_empleados PRIMARY KEY(dni)
+  );
 
-alter table empleados
-add constraint pk_empleados
-primary key(DNI);
+ALTER TABLE empleados
+  ALTER COLUMN dni VARCHAR(10) NOT NULL;
+go
+ALTER TABLE empleados
+  ADD CONSTRAINT pk_empleados PRIMARY KEY(dni);
+
 go
 
 -- b. no nulo appelidos
 -- Primera manera
-create table empleados (
-apellidos varchar(30) NOT NULL
-);
+CREATE TABLE empleados
+  (
+     apellidos VARCHAR(30) NOT NULL
+  );
 
 -- Segunda manera
+ALTER TABLE empleados
+  ALTER COLUMN apellidos VARCHAR(30) NOT NULL;
 
-alter table empleados
-alter column apellidos varchar(30) NOT NULL;
 go
 
 -- c. no nulo nombre
 -- Primera manera
-create table empleados (
-nombre varchar(30) NOT NULL
-);
+CREATE TABLE empleados
+  (
+     nombre VARCHAR(30) NOT NULL
+  );
 
 -- Segunda manera
+ALTER TABLE empleados
+  ALTER COLUMN nombre VARCHAR(30) NOT NULL;
 
-alter table empleados
-alter column nombre varchar(30) NOT NULL;
 go
 
 -- d. valor unico nombre y apellidos
 -- Primera manera
-
-create table empleados (
-nombre varchar(30) UNIQUE,
-apellidos varchar(30) UNIQUE
-);
+CREATE TABLE empleados
+  (
+     nombre    VARCHAR(30) UNIQUE,
+     apellidos VARCHAR(30) UNIQUE
+  );
 
 -- Segunda manera
-
-create table empleados (
-nombre varchar(30),
-apellidos varchar(30),
-constraint uq_nombreapellidos 
-	unique (nombre, apellidos)
-);
+CREATE TABLE empleados
+  (
+     nombre    VARCHAR(30),
+     apellidos VARCHAR(30),
+     CONSTRAINT uq_nombreapellidos UNIQUE (nombre, apellidos)
+  );
 
 -- Tercera manera
-
-alter table empleados
-add constraint uq_nombreapellidos
-	unique (nombre, apellidos);
+ALTER TABLE empleados
+  ADD CONSTRAINT uq_nombreapellidos UNIQUE (nombre, apellidos);
 
 --e. validar que fechanacimiento sea menor que la fecha actual
 -- Primera manera
-
-create table empleados (
-DNI varchar(8),
-nombre varchar(30),
-apellidos varchar(30),
-fechanacimiento datetime,
-cantidadhijos tinyint,
-seccion varchar(20),
-sueldo decimal(6,2),
-constraint ck_fechas
-	check(fechanacimiento < getdate())
-);
+CREATE TABLE empleados
+  (
+     fechanacimiento DATETIME,
+     CONSTRAINT ck_fechas CHECK(fechanacimiento < Getdate())
+  );
 
 -- Segunda manera
-
-alter table empleados
-add constraint ck_fechas
-	check(fechanacimiento < getdate());
+ALTER TABLE empleados
+  ADD CONSTRAINT ck_fechas CHECK(fechanacimiento < Getdate());
 
 --f. validar que cantidad de hijos no sea negativa ni mayor que 20
 -- Primera manera
-
-create table empleados (
-DNI varchar(8),
-nombre varchar(30),
-apellidos varchar(30),
-fechanacimiento datetime,
-cantidadhijos tinyint,
-seccion varchar(20),
-sueldo decimal(6,2),
-constraint ck_canthijos
-	check(cantidadhijos < 20 AND cantidadhijos > 0)
-);
+CREATE TABLE empleados
+  (
+     cantidadhijos   TINYINT,
+     CONSTRAINT ck_canthijos CHECK(cantidadhijos < 20 AND cantidadhijos > 0)
+  );
 
 -- Segunda manera
-
-alter table empleados
-add constraint ck_canthijos
-	check(cantidadhijos < 20 AND cantidadhijos > 0);
+ALTER TABLE empleados
+  ADD CONSTRAINT ck_canthijos CHECK(cantidadhijos < 20 AND cantidadhijos > 0);
 
 --g. validar que sección no esté vacío
 -- Primera manera
-
-create table empleados (
-DNI varchar(8),
-nombre varchar(30),
-apellidos varchar(30),
-fechanacimiento datetime,
-cantidadhijos tinyint,
-seccion varchar(20),
-sueldo decimal(6,2),
-constraint ck_seccionempty
-	check(trim(seccion) != '')
-);
+CREATE TABLE empleados
+  (
+     seccion         VARCHAR(20),
+     CONSTRAINT ck_seccionempty CHECK(Trim(seccion) != '')
+  );
 
 -- Segunda manera
-
-alter table empleados
-add constraint ck_seccionempty
-	check(trim(seccion) != '');
-
+ALTER TABLE empleados
+  ADD CONSTRAINT ck_seccionempty CHECK(Trim(seccion) != '');
 
 -- VALIDACIONES DE ESTAS RESTRICCIONES
+INSERT INTO empleados
+            (nombre,
+             apellidos)
+VALUES      ('Pepe',
+             'Pepon');
 
-insert into empleados(nombre, apellidos)
-values ('Pepe', 'Pepon');
 go
 
-insert into empleados(DNI, nombre, apellidos)
-values ('12345678', 'Pepe', 'Pepon');
+INSERT INTO empleados
+            (dni,
+             nombre,
+             apellidos)
+VALUES      ('12345678',
+             'Pepe',
+             'Pepon');
+
 go
 
 --4. Ver los índices que tiene.
+EXEC Sp_helpindex
+  empleados;
 
-exec sp_helpindex empleados;
 go
 
 --5. Añadir índice por fecha de nacimiento
-create nonclustered index ix_fechanacimiento
-on empleados(fechanacimiento);
+CREATE NONCLUSTERED INDEX ix_fechanacimiento
+  ON empleados(fechanacimiento);
+
 go
 
 --6. Añadir índice por sueldo
-create nonclustered index ix_sueldo
-on empleados(sueldo)
+CREATE NONCLUSTERED INDEX ix_sueldo
+  ON empleados(sueldo)
+
 go
 
 --7. Modificar lo siguiente en la tabla
 --a. Añadir campo dirección varchar(100)
-alter table empleados
-add direccion varchar(100);
+ALTER TABLE empleados
+  ADD direccion VARCHAR(100);
+
 go
 
 --b. Cambiar a no nulo seccion
-alter table empleados
-alter column seccion varchar(20) NOT NULL;
+ALTER TABLE empleados
+  ALTER COLUMN seccion VARCHAR(20) NOT NULL;
+
 go
 
 --c. Validar que sueldo sean >0 y <10000
-alter table empleados
-add constraint CK_Sueldobwt
-	check(seccion BETWEEN 0 AND 10000);
-go
+ALTER TABLE empleados
+  ADD CONSTRAINT ck_sueldobwt CHECK(seccion BETWEEN 0 AND 10000);
+
+go 
